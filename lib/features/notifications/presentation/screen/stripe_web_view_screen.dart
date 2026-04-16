e4689a1e13cacfffc12be1f2c55194ad1219a96a
MD Naim Mia <developernaimul00@gmail.com>
2026-04-16 09:51:19 +0600

    done

--- lib/features/notifications/presentation/screen/stripe_web_view_screen.dart
+++ lib/features/notifications/presentation/screen/stripe_web_view_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:giolee78/features/ads/presentation/screen/create_ads_screen.dart';
 import 'package:giolee78/features/home/presentation/screen/home_nav_screen.dart';
 import 'package:giolee78/utils/constants/app_colors.dart';
 import 'package:webview_flutter/webview_flutter.dart';
